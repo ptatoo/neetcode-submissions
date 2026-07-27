5e6d692f9ebab50e8448b79d380b4b8269d17fb1
Alexander Song <88013429+ptatoo@users.noreply.github.com>
2026-07-27 14:54:14 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashMap = {}
+
+        for i, s in enumerate(strs):
+            c1 = Counter(s)
+            found = False
+            for key in hashMap.keys():
+                c2 = Counter(key)
+                if c1 == c2:
+                    hashMap[key].append(s)
+                    found = True
+            if not found:
+                hashMap[s] = [s]
+
+        output = []
+
+        for key in hashMap.keys():
+            output.append(hashMap[key])
+
+        return output
