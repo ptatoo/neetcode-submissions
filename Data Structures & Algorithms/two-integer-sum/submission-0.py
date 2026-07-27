db608e1971b5a8b428b3373895ecfef9daae2eda
Alexander Song <88013429+ptatoo@users.noreply.github.com>
2026-07-27 14:18:52 -0400

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashIdx = {}
+        hashMap = {}
+
+        for idx, n in enumerate(nums):
+            if n in hashMap:
+                hashMap[n] += 1
+            else:
+                hashMap[n] = 1
+
+        for idx, n in enumerate(nums):
+            reverse = target - n
+            if (n == reverse):
+                if n in hashMap and hashMap[n] == 2:
+                    indices = [i for i, x in enumerate(nums) if x == n]
+                    return [min(indices[0], indices[1]), max(indices[0], indices[1])]
+            else:
+                if n in hashMap and hashMap[n] == 1 and reverse in hashMap and hashMap[reverse] == 1:
+                    idx2 = nums.index(reverse)
+                    return [min(idx, idx2), max(idx, idx2)]
+        
+        return [-1, -1]
